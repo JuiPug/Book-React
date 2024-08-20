e3a88f56e6060262ff1a18c7b1f6e60852d84703
nguye <nguye@WINDOWS>
2024-08-20 23:35:03 +0700

    folder3ComponentFeature

--- src/index.js
+++ src/index.js
@@ -50,10 +50,14 @@ const EventExamples = () => {
 }
 const Book = (props) => {
   console.log(props)
+  const disPlayTitle = () => {
+    console.log(props.title)
+  }
   return (
     <article className="book">
       <img src={props.img} alt={props.title} />
       <h2>{props.title}</h2>
+      <button onClick={disPlayTitle}>Dislay Title</button>
       <h4>{props.author}</h4>
     </article>
   )
