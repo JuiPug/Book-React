1c69c2501a09815b4fd482c068ec13f223845383
nguye <nguye@WINDOWS>
2024-08-20 22:46:05 +0700

    folder3eventhandle

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+const books = [
+  {
+    author: 'Colleen Hoover',
+    title: 'It Ends with Us',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/813aV273-rL._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Alex Hirsch',
+    title: 'The Book of Bill (Gravity Falls)',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81O6JfipQhL._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+]
 
-const firstBook = {
-  author: 'Colleen Hoover',
-  title: 'It Ends with Us',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/813aV273-rL._AC_UL600_SR600,400_.jpg',
-}
-const secondBook = {
-  author: 'Alex Hirsch',
-  title: 'The Book of Bill (Gravity Falls)',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81O6JfipQhL._AC_UL600_SR600,400_.jpg',
-}
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      <EventExamples />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />
+      })}
+    </section>
+  )
+}
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('Handle form input')
+  }
+  const handleButtonClick = () => {
+    alert('Handle button click')
+  }
+  return (
+    <section>
+      <form>
+        <h2>Typical form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click</button>
     </section>
   )
 }
